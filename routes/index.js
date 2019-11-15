83c9eec5c473ac229668e6980523efd1da873b90
Thomas Kerin <me@thomaskerin.io>
2019-11-15 17:03:36 +0000

    use sinon to mock Model methods

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Blog = require("../models/blog.model");
 
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
     Blog.find(function (err, docs) {
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,15 @@
 var request = require('supertest');
+const sinon = require('sinon');
+var Blog = require("../models/blog.model");
+
+function expectBodyIncludes(stringToMatch) {
+    return function (res) {
+        if (!(res.text.includes(stringToMatch))) {
+            throw new Error("expected response body to contain '"+stringToMatch+"'")
+        }
+    }
+}
+
 describe('loading express', function () {
     var app;
     var server;
@@ -15,9 +26,12 @@ describe('loading express', function () {
         done()
     });
     it('responds to /', function testSlash(done) {
+        var emptyBlogList = [];
+        sinon.stub(Blog, 'find').yields(null, emptyBlogList);
         request(server)
             .get('/')
-            .expect(200, done);
+            .expect(200, done)
+            .expect(expectBodyIncludes("Welcome to the Irish Cactus and Succulent Society Website"));
     });
     it('404 everything else', function testPath(done) {
         request(server)
