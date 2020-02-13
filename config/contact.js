c6f59f55c56f7c42947a938548056aa725b30ab6
Thomas Kerin <me@thomaskerin.io>
2020-02-13 01:14:43 +0000

    use societyFullName in index

--- config/contact.js
+++ config/contact.js
@@ -2,7 +2,7 @@ module.exports = {
     abbreviation: "ICSS",
     mobile: "(+353) 87-2308330",
     emailAddress: "irishcactusandsucculentsociety@gmail.com",
-    fullName: "The Irish Cactus and Succulent Society",
+    fullName: "Irish Cactus and Succulent Society",
     addressHtmlFormat: "Irish Cactus and Succulent Society<br />\n" +
         "                    Dublin Botanic Gardens<br />\n" +
         "                    Glasnevin, Dublin 9<br />\n" +
